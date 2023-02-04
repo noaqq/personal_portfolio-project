fc5576b062650f7b7d4b220bc88cefd4074a18fb
noaqq <maximbaymetov2002@icloud.com>
2023-02-04 18:38:12 +0300

    add blog page

--- personal_portfolio/blog/views.py
+++ personal_portfolio/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Blog
+
+
+def all_blogs(request):
+    blogs = Blog.objects.all()
+    return render(request, "blog/all_blogs.html", {"blogs": blogs})
--- personal_portfolio/personal_portfolio/urls.py
+++ personal_portfolio/personal_portfolio/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from portfolio import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name = "home"),
+    path('blog/', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT )
